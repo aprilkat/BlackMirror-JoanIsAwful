73f01f2147c115edc089eb9ac4aa8c9ad008717e
April Bollinger <63476704+aprilkat@users.noreply.github.com>
2024-04-17 16:37:32 -0400

    padding

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
+
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
